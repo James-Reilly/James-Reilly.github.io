bdafe094ba3931c513a8cf3dd85b89207f7bc859
James Michael Reilly <jmr8893@rit.edu>
2015-02-16 17:05:34 -0500

    Added Project Posts
    Tweet and SimpleCalc

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 /**
  * Created by jreilly on 2/10/15.
  */
+
+Polymer({});
 var app = document.querySelector('#app');
 app.page = 'about';
+console.log("Scripts! amirite? ");
+
+var tabs = document.querySelector('#tabs');
+var list = document.querySelector('post-list');
+  tabs.addEventListener('core-select', function() {
+  	console.log("Selected: " + tabs.selected);
+    list.show = tabs.selected;
+ });
 
 app.menuItemSelected = function(e, detail, sender) {
     if (detail.isSelected) {
