a134f3aeb4d9077c88b9d00b7ebb9e188bfcc085
James Michael Reilly <jmr8893@rit.edu>
2015-07-09 22:20:38 -0400

    Adding re-done website

---
+++ index.js
@@ -0,0 +1,10 @@
+$( document ).ready(function(){
+    $(".button-collapse").sideNav();
+    $('.scrollspy').scrollSpy();
+
+    $('.toc-wrapper').pushpin({ top: 0 });
+    $('.parallax').parallax();
+    $('ul.tabs').tabs();
+    console.log("its alaive!");
+
+ });
